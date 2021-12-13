1d7f94d16595497443833ae785045ecad25f737c
Phil Livermore <p.w.livermore@leeds.ac.uk>
2021-12-13 15:38:34 +0000

    Delta slope updates

--- Fortran/Jerks.F90
+++ Fortran/Jerks.F90
@@ -30,9 +30,10 @@ MODULE Jerks
 !
 ! discretise_size: the number of grid points that defines the resolution of some of the outputs (e.g. ensemble mean, median timeseries).
 !
-! cp_nbins: the number of bins defining the histogram of internal vertices (change points)
+! time_intervals_nbins: the number of bins used to define diagnostics such as the histogram of internal vertices (change points), or the normalised change in slope
 
-! cp_edges: the edges of the bins defining the histogram of internal vertices (change points). Note that the size of cp_edges should be cp_bins + 1
+! time_intervals_edges: the edges of the bins used to define diagnostics such as the histogram of internal vertices (change points), or the normalised change in slope.
+! Note that the size of time_intervals_edges should be time_intervals_nbins + 1
 !
 ! thin: the amount of chain thinning. E.g. a value of n means that statistics are only evaluated only at every nth model
 !
@@ -58,13 +59,16 @@ MODULE Jerks
 !
 ! ENSEMBLE_MODE: the ensemble modal model (array size: discretize_size)
 !
-! CHANGE_POINTS: a histogram of the timing of internal vertices (array, size CP_BINS)
+! CHANGE_POINTS: a histogram of the timing of internal vertices (array, size time_intervals_nbins)
 ! Each model can add at most 1 to each bin; so normalised over all models this is a posterior probability.
 !
 ! MARGINAL_DENSITY: a 2D marginal density for the linear ensemble or zeros (if not calculated); (array, size: discretise_size,NBINS)
 !
 ! N_changepoint_hist: a histogram of the number of internal vertices (1D array, dimensions K_MIN:K_MAX)
-
+!
+! delta_slope : the sum of the absolute values of any change in slope over all models occuring during a given time bin; normalised by the number of models.
+! That is, \sum_i  abs(delta slope), where i is over all models and over a certain temporal bin defined in the input vector time_intervals_edges.
+! (1D array, size: time_intervals_nbins)
 
 USE SORT
 USE SUBS
@@ -72,10 +76,10 @@ USE SUBS
 CONTAINS
 SUBROUTINE RJMCMC(sigmas, burn_in, NSAMPLE, NUM_DATA, TIMES, &
            Y, delta_Y, Y_MIN, Y_MAX, TIMES_MIN, &
-           TIMES_MAX, K_MIN, K_MAX, discretise_size, CP_NBINS, CP_EDGES, &
+           TIMES_MAX, K_MIN, K_MAX, discretise_size, time_intervals_nbins, time_intervals_edges, &
            THIN, NBINS, credible, RUNNING_MODE, &
            Acceptance_rates, CREDIBLE_SUP, CREDIBLE_INF, ENSEMBLE_AV, ENSEMBLE_MEDIAN, &
-           ENSEMBLE_MODE, CHANGE_POINTS, MARGINAL_DENSITY, &
+           ENSEMBLE_MODE, CHANGE_POINTS, delta_slope,  MARGINAL_DENSITY, &
            N_changepoint_hist )
 
 
@@ -90,18 +94,20 @@ real(dp), intent(in) :: Y_MIN, Y_MAX, TIMES_MIN, TIMES_MAX, credible
 integer, intent(in)   :: thin, NBINS, K_MIN, K_MAX, discretise_size
 integer, intent(in) :: NSAMPLE
 real(dp), intent(in) :: sigmas(3)
-INTEGER, intent(in) :: CP_NBINS
-real(dp), intent(in) :: CP_EDGES(CP_NBINS + 1)
+INTEGER, intent(in) :: time_intervals_nbins
+real(dp), intent(in) :: time_intervals_edges(time_intervals_nbins + 1)
+
 
 ! OUTPUTS
 real(dp), intent(out) :: Acceptance_rates(4)
 REAL( KIND = dp), DIMENSION(1:discretise_size), intent(out) :: ENSEMBLE_AV, CREDIBLE_SUP, &
         CREDIBLE_INF, ENSEMBLE_MEDIAN, ENSEMBLE_MODE
-INTEGER, intent(out) :: CHANGE_POINTS(CP_NBINS)
+INTEGER, intent(out) :: CHANGE_POINTS(time_intervals_nbins)
 
 REAL( KIND = dp), intent(out) :: MARGINAL_DENSITY(discretise_size,NBINS)
 INTEGER, intent(out) :: N_changepoint_hist(K_MIN:K_MAX)
 
+REAL( KIND = dp), intent(out) :: delta_slope(time_intervals_nbins)
 
 ! Internal variables
 
@@ -116,7 +122,10 @@ INTEGER, ALLOCATABLE :: discrete_history(:,:)
 LOGICAL :: CALC_CREDIBLE
 INTEGER :: input_random_seed
 INTEGER, ALLOCATABLE :: SEED(:)
-INTEGER :: BIN_UPDATES(CP_NBINS)
+INTEGER :: BIN_COUNT(time_intervals_nbins)
+REAL(dp) :: fn(1), fn_plus_1(1), fn_minus_1(1), X(1)
+
+REAL(dp), PARAMETER :: delta_time_slope = 1.0e-6   !perturbation used to measure slopes
 
 
 IF( credible > 0.0_dp) THEN
@@ -175,6 +184,7 @@ PA = 0
 P_sd = 0
 A_sd = 0
 AA = 0
+delta_slope(:) = 0.0_dp
 
 ! initial condition
 CALL RANDOM_NUMBER( RAND(1))
@@ -544,20 +554,35 @@ N_changepoint_hist(k)=N_changepoint_hist(k) + 1
 !calculate the histogram on change points, adding a 1 to the bin if there is a change point.
 !Only allow at most one changepoint in any bin - this is so the bin count normalised by the number of models is the probability of a changepoint.  
 
+! Having found the correct bin, add in the absolute change in slope. NB away from a change point this is zero.
 ! zero the update vector
-BIN_UPDATES(1:CP_NBINS) = 0
+BIN_COUNT(1:time_intervals_nbins) = 0
 
 DO i=1, k !loop over all change points
-DO j = 1, CP_NBINS
-    IF( pt(i,1) >= CP_EDGES(j) .AND. pt(i,1) < CP_EDGES(j+1) ) THEN
-        BIN_UPDATES(j) = 1
-        EXIT
+DO j = 1, time_intervals_nbins
+    IF( pt(i,1) >= time_intervals_edges(j) .AND. pt(i,1) < time_intervals_edges(j+1) ) THEN  ! relevant bin found
+        BIN_COUNT(j) = 1
+
+! Find delta slope by using a small perturbation
+    X(1) = pt(i,1)
+    CALL Find_linear_interpolated_values( k, TIMES_min, TIMES_max, pt, endpt, 1, X, fn)   !the function at a point x_n
+    X(1) = pt(i,1) + delta_time_slope
+    CALL Find_linear_interpolated_values( k, TIMES_min, TIMES_max, pt, endpt, 1, X, fn_plus_1) !the function at a point x_{n+1}
+    X(1) = pt(i,1) - delta_time_slope
+    CALL Find_linear_interpolated_values( k, TIMES_min, TIMES_max, pt, endpt, 1, X, fn_minus_1) !the function at a point x_{n-1}
+
+! Change in slope is approximated by abs(  f(x_{n+1}) - f(x_{n})/delta_time_slope  - ( f(x_n) - f(x_{n-1}) ) /delta_time_slope )
+
+        delta_slope(j) = delta_slope(j) + abs( (fn_plus_1(1) + fn_minus_1(1) - 2.0_dp * fn(1))/delta_time_slope )
+
+
+        EXIT  ! now try another change point
     ENDIF
 ENDDO
 ENDDO
 
-CHANGE_POINTS(1:CP_NBINS) = CHANGE_POINTS(1:CP_NBINS) + BIN_UPDATES(1:CP_NBINS)
-
+! Add in the update to the bin counts
+CHANGE_POINTS(1:time_intervals_nbins) = CHANGE_POINTS(1:time_intervals_nbins) + BIN_COUNT(1:time_intervals_nbins)
 
 
 ENDIF !collect stats
@@ -566,6 +591,8 @@ enddo ! the Sampling of the mcmc
 
 ! Compute the average
 ENSEMBLE_AV(:)=ENSEMBLE_AV(:)/b
+
+delta_slope(:) = delta_slope(:) / b
 
 do i=1, discretise_size
 
